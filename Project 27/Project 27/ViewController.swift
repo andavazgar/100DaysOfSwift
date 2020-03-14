83dd4a0fac3e41744b491677852316a8d77c5dc8
Andrés Vazquez <andavazgar.dev@gmail.com>
2020-03-13 23:57:28 -0400

    (Day 88) Project 27

---
+++ Project 27/Project 27/ViewController.swift
@@ -0,0 +1,139 @@
+//
+//  ViewController.swift
+//  Project 27
+//
+//  Created by Andres Vazquez on 2020-03-12.
+//  Copyright © 2020 Andavazgar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+    var currentDrawType = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        drawRectangle()
+    }
+
+    @IBAction func redrawBtnTapped(_ sender: UIButton) {
+        currentDrawType = (currentDrawType + 1) % 6
+        
+        switch currentDrawType {
+        case 0:
+            drawRectangle()
+        case 1:
+            drawCircle()
+        case 2:
+            drawCheckerboard()
+        case 3:
+            drawRotatedSquares()
+        case 4:
+            break
+        case 5:
+            break
+        default:
+            break
+        }
+    }
+    
+    private func drawRectangle() {
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
+        let image = renderer.image { context in
+            let rectangle = CGRect(x: 0, y: 0, width: 512, height: 512).insetBy(dx: 5, dy: 5)
+            
+            context.cgContext.setFillColor(UIColor.red.cgColor)
+            context.cgContext.setStrokeColor(UIColor.black.cgColor)
+            context.cgContext.setLineWidth(10)
+            
+            context.cgContext.addRect(rectangle)
+            context.cgContext.drawPath(using: .fillStroke)
+        }
+        
+        imageView.image = image
+    }
+    
+    private func drawCircle() {
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
+        let image = renderer.image { context in
+            let rectangle = CGRect(x: 0, y: 0, width: 512, height: 512).insetBy(dx: 5, dy: 5)
+            
+            context.cgContext.setFillColor(UIColor.red.cgColor)
+            context.cgContext.setStrokeColor(UIColor.black.cgColor)
+            context.cgContext.setLineWidth(10)
+            
+            context.cgContext.addEllipse(in: rectangle)
+            context.cgContext.drawPath(using: .fillStroke)
+        }
+        
+        imageView.image = image
+    }
+    
+    private func drawCheckerboard() {
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
+        let image = renderer.image { context in
+            context.cgContext.setFillColor(UIColor.black.cgColor)
+            
+            for row in 0 ..< 8 {
+                for col in 0 ..< 8 {
+                    if (row + col) % 2 == 0 {
+                        context.cgContext.fill(CGRect(x: col * 64, y: row * 64, width: 64, height: 64))
+                    }
+                }
+            }
+        }
+        
+        imageView.image = image
+    }
+    
+    private func drawRotatedSquares() {
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
+        let image = renderer.image { context in
+            let numOfRotations = 16
+            let amountToRotate = Double.pi / Double(numOfRotations)
+            
+            context.cgContext.translateBy(x: 256, y: 256)
+            
+            for _ in 0 ..< numOfRotations {
+                context.cgContext.rotate(by: CGFloat(amountToRotate))
+                context.cgContext.addRect(CGRect(x: -128, y: -128, width: 256, height: 256))
+            }
+            
+            context.cgContext.setStrokeColor(UIColor.black.cgColor)
+            context.cgContext.strokePath()
+        }
+        
+        imageView.image = image
+    }
+    
+    private func drawLines() {
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
+        let image = renderer.image { context in
+            context.cgContext.translateBy(x: 256, y: 256)
+            
+            var first = true
+            var length: CGFloat = 256
+            
+            for _ in 0 ..< 256 {
+                context.cgContext.rotate(by: .pi / 2)
+                
+                if first {
+                    context.cgContext.move(to: CGPoint(x: length, y: 50))
+                    first = false
+                } else {
+                    context.cgContext.addLine(to: CGPoint(x: length, y: 50))
+                }
+                
+                length *= 0.99
+            }
+            
+            context.cgContext.setStrokeColor(UIColor.black.cgColor)
+            context.cgContext.strokePath()
+        }
+        
+        imageView.image = image
+    }
+}
+
